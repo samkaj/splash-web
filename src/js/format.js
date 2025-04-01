30662abf1f2efc732e6b4e29c600a26b6f38172b
samkaj <kajavasamuel@gmail.com>
2025-04-01 21:16:48 +0200

    [feat] client side generator API call in place

---
+++ src/js/format.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const output = document.querySelector('#output');
+
+const loadGenerator = () => {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const data = new FormData(form);
+
+        try {
+            const response = await fetch('/generate', {
+                method: 'POST',
+                body: data,
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.text();
+            output.textContent = result;
+        } catch (error) {
+            console.error('Fetch error:', error);
+            output.textContent = 'Error occurred.';
+        }
+    });
+}
+
+export default loadGenerator;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import loadColors from "./colors.js";
+import loadGenerator from "./format.js";
 document.addEventListener("DOMContentLoaded", () => {
     loadColors();
+    loadGenerator();
 });
